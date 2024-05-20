3438610117c78444280175be63377769df3b3c28
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-05-20 16:54:10 -0300

    chore: Update styling and functionality for goal creation and deletion

--- App.js
+++ App.js
@@ -18,27 +18,29 @@ export default function App() {
     setCurrentGoalText("");
   };
 
-  const handleDeleteGoal = () => {
-    setGoals((prevGoals) => {
-      return prevGoals.filter((goal) => goal !== currentGoalText);
-    });
-  };
-
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <View>
+      <StatusBar style="light" />
+      <Text style={styles.title}>Course Native Goals</Text>
+      <View style={styles.userInputs}>
         <TextInput
           style={styles.input}
           value={currentGoalText}
           onChangeText={(text) => setCurrentGoalText(text)}
           placeholder="Enter your new goal"
+          placeholderTextColor="#fff"
         />
         <Button onPress={() => handleAddGoal()} title="Create" />
       </View>
-      <View style={styles.goals}>
+      <View style={styles.goalsContainer}>
         {goals.map((goal) => (
-          <Text onPress={() => handleDeleteGoal()} key={goal}>
+          <Text
+            style={styles.goalText}
+            key={goal}
+            onPress={() =>
+              setGoals((prevGoals) => prevGoals.filter((g) => g !== goal))
+            }
+          >
             {goal}
           </Text>
         ))}
@@ -50,27 +52,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333",
+    backgroundColor: "#111",
     alignItems: "center",
+    paddingTop: 50,
   },
   userInputs: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "80%",
+    width: "70%",
+    gap: 20,
     flex: 1,
   },
-  goals: {
+  goalsContainer: {
     flex: 3,
     justifyContent: "center",
     alignItems: "center",
     width: "80%",
+    marginTop: 60,
   },
   input: {
-    borderBottomColor: "black",
+    borderBottomColor: "#fff",
     borderBottomWidth: 1,
     width: "80%",
-    color: "white",
+    color: "#fff",
     marginBottom: 20,
+    marginTop: 20,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+  },
+  goalText: {
+    color: "#fff",
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius: 5,
+    marginBottom: 10,
   },
 });
